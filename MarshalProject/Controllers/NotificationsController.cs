9e50fb5f17ef7cab3a89a38d21af3d51ca815a50
Ahassnat <alhassnatabdallah@gmail.com>
2021-11-23 21:27:42 +0200

    Add Notification Control To Notification Branch

---
+++ MarshalProject/Controllers/NotificationsController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MarshalProject.Data;
+using MarshalProject.Hubs;
+using MarshalProject.ViewModel.Notification;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarshalProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly IHubContext<BroadcastHub, IHubClient> _hubContext;
+
+        public NotificationsController(DataContext context, IHubContext<BroadcastHub, IHubClient> hubContext)
+        {
+            _context = context;
+            _hubContext = hubContext;
+        }
+
+        // GET: api/Notifications/notificationcount  
+        [Route("notificationcount")]
+        [HttpGet]
+        public async Task<ActionResult<NotificationCountResult>> GetNotificationCount()
+        {
+            var count = (from not in _context.Notifications
+                         select not).CountAsync();
+            NotificationCountResult result = new NotificationCountResult
+            {
+                Count = await count
+            };
+            return result;
+        }
+
+        // GET: api/Notifications/notificationresult  
+        [Route("notificationresult")]
+        [HttpGet]
+        public async Task<ActionResult<List<NotificationResult>>> GetNotificationMessage()
+        {
+            var results = from message in _context.Notifications
+                          orderby message.Id descending
+                          select new NotificationResult
+                          {
+                              UserName = message.UserName,
+                              TranType = message.TranType
+                          };
+            return await results.ToListAsync();
+        }
+
+        // DELETE: api/Notifications/deletenotifications  
+        [HttpDelete]
+        [Route("deletenotifications")]
+        public async Task<IActionResult> DeleteNotifications()
+        {
+            await _context.Database.ExecuteSqlRawAsync("TRUNCATE TABLE Notification");
+            await _context.SaveChangesAsync();
+            await _hubContext.Clients.All.BroadcastMessage();
+
+            return NoContent();
+        }
+    }
+}
+
